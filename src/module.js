6747469f790f1972c1733de2f07fce4927ace469
Alex Parrill <aparrill@datto.com>
2020-05-06 11:24:55 -0400

    Workaround grafana issue #21785

--- src/module.js
+++ src/module.js
@@ -1,5 +1,9 @@
 import {BosunAppConfigCtrl} from './components/config';
 
+// Import _something_ to populate a dependency and fix
+// https://github.com/grafana/grafana/issues/21785
+import TableModel from 'grafana/app/core/table_model';
+
 export {
   BosunAppConfigCtrl as ConfigCtrl
 };
